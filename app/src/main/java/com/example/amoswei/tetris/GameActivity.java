9569773e98ea4a538030343d165749257f2bdfad
Wangchao  Wei <u6263937@anu.edu.au>
2018-05-12 20:48:50 +1000

    hide NavigationBar

--- app/src/main/java/com/example/amoswei/tetris/GameActivity.java
+++ app/src/main/java/com/example/amoswei/tetris/GameActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
 
 import static com.example.amoswei.tetris.GameOverActivity.GAMEOVER_MESSAGE;
@@ -25,5 +26,31 @@ public class GameActivity extends AppCompatActivity implements GameOver {
         Intent gameOverIntent = new Intent(this, GameOverActivity.class);
         startActivity(gameOverIntent);
         finish();
+    }
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if( hasFocus ) {
+            hideNavigationBar();
+        }
+    }
+    private void hideNavigationBar() {
+        // TODO Auto-generated method stub
+        final View decorView = getWindow().getDecorView();
+        final int flags = View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+        decorView.setSystemUiVisibility(flags);
+        decorView.setOnSystemUiVisibilityChangeListener(new View.OnSystemUiVisibilityChangeListener() {
+            @Override
+            public void onSystemUiVisibilityChange(int visibility) {
+                if ((visibility & View.SYSTEM_UI_FLAG_FULLSCREEN) == 0) {
+                    decorView.setSystemUiVisibility(flags);
+                }
+            }
+        });
     }
 }
